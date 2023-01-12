2ab426e1a14f937eac1df7e5aa695db42dc562f3
Craig Abbott <admin@craigabbott.co.uk>
2023-01-12 20:08:46 +0000

    Added lightbox

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,15 @@
 module.exports = function (eleventyConfig, pluginNamespace) {
   eleventyConfig.namespace(pluginNamespace, () => {
-    eleventyConfig.addShortcode('respimg', (path, alt, sizes, classList) => {
+    eleventyConfig.addShortcode('respimg', (path, alt, sizes, classList, lightbox=false) => {
       const fetchBase = `https://res.cloudinary.com/${eleventyConfig.cloudinaryCloudName}/image/fetch/`;
       const src = `${fetchBase}q_auto,f_auto,w_${eleventyConfig.fallbackWidth}/${path}`;
       const srcset = eleventyConfig.srcsetWidths.map(w => {
         return `${fetchBase}q_auto,f_auto,w_${w}/${path} ${w}w`;
       }).join(', ');
 
+      if (lightbox) {
+        return `<a href="${path}"><img src="${src}" srcset="${srcset}" sizes="${sizes ? sizes : '100vw'}" alt="${alt ? alt : ''}" ${classList ? 'class="' + classList + '"' : ""} ${eleventyConfig.lazyLoad ? 'loading="lazy"' : ""}></a>`;
+      }
       return `<img src="${src}" srcset="${srcset}" sizes="${sizes ? sizes : '100vw'}" alt="${alt ? alt : ''}" ${classList ? 'class="' + classList + '"' : ""} ${eleventyConfig.lazyLoad ? 'loading="lazy"' : ""}>`;
     });
   });
